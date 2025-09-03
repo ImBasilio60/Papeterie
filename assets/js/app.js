ea026d7a5d0df9e4e18a6f87d7c85d2bcab9c8e8
Basilio <basilioherimiharisoa@gmail.com>
2025-09-03 06:10:13 +0300

    Responsive modification

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,5 @@
 const btn = document.querySelector(".burger-toggle");
 const icon = btn.firstElementChild;
-btn.addEventListener("click", () => {
-  if (icon.classList.contains("fa-bars")) {
-    icon.classList.remove("fa-bars");
-    icon.classList.add("fa-times");
-  } else {
-    icon.classList.add("fa-bars");
-    icon.classList.remove("fa-times");
-  }
-  document.querySelector("nav.navigation").classList.toggle("active");
-  document.querySelector(".header").classList.toggle("active");
-});
 
 window.addEventListener("resize", (e) => {
   icon.classList.add("fa-bars");
@@ -18,3 +7,16 @@ window.addEventListener("resize", (e) => {
   document.querySelector("nav.navigation").classList.remove("active");
   document.querySelector(".header").classList.remove("active");
 });
+if (window.innerWidth < 1024) {
+  btn.addEventListener("click", () => {
+    if (icon.classList.contains("fa-bars")) {
+      icon.classList.remove("fa-bars");
+      icon.classList.add("fa-times");
+    } else {
+      icon.classList.add("fa-bars");
+      icon.classList.remove("fa-times");
+    }
+    document.querySelector("nav.navigation").classList.toggle("active");
+    document.querySelector(".header").classList.toggle("active");
+  });
+}
